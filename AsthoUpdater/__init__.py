483187deb5d1e5de62ee366a194a75bcb1951f3b
Raphael Pilinski <raphael.pilinski@protonmail.com>
2021-04-07 20:22:14 +0200

    Update to 0.1.2

--- AsthoUpdater/__init__.py
+++ AsthoUpdater/__init__.py
@@ -1,6 +1,6 @@
-from AsthoUpdater.AsthoUpdater import AsthoUpdater
+from .AsthoUpdater import AsthoUpdater
 
 __title__ = "AsthoUpdater"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Asthowen"
 __license__ = "Mozilla Public License 2.0"
