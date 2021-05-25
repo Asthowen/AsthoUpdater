762eae34638404a1cd560f7c460a4ebc5ab421f8
Asthowen <raphael.pilinski@protonmail.com>
2021-05-25 13:00:34 +0200

    Add GitHub Actions to auto publish AsthoUpdater on PyPI and Test PyPI.

--- AsthoUpdater/__init__.py
+++ AsthoUpdater/__init__.py
@@ -1,6 +1,6 @@
 from .AsthoUpdater import AsthoUpdater
 
 __title__ = "AsthoUpdater"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Asthowen"
 __license__ = "Mozilla Public License 2.0"
